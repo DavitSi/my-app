7c6144e770337bf10d77d20853114c19e70163ff
davitsi <davit.smn@gmail.com>
2019-10-08 22:42:25 +0400

    Detail container re-arranging

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -40,7 +40,39 @@ class Detail extends React.Component {
             )
         }
         return (
-            <div>Hello detail</div>
+            <div className="Detail">
+                <h1 className="Detail-heading">
+                    {currency.name} ({currency.symbol})
+                </h1>
+                <div className="Detail-container">
+                    <div className="Detail-item">
+                        Price <span className="Detail-value">$ {currency.price}</span>
+                    </div>
+                    <div className="Detail-item">
+                        Rank <span className="Detail-value">{currency.rank}</span>
+                    </div>
+                    <div className="Detail-item">
+                        24H change
+                        <span className="Detail-value">
+                           {handleChangePercent(currency.percentChange24h)}
+                        </span>
+                    </div>
+                    <div className="Detail-item">
+                        <span className="Detail-title">Market cap</span>
+                        <span className="Detail-dollar">$</span>
+                        {currency.marketCap}
+                    </div>
+                    <div className="Detail-item">
+                        <span className="Detail-title">24H Volume</span>
+                        <span className="Detail-dollar">$</span>
+                        {currency.volume24h}
+                    </div>
+                    <div className="Detail-item">
+                        <span className="Detail-title">Total supply</span>
+                        {currency.totalSupply}
+                    </div>
+                </div>
+            </div>
         );
     };
 }
